b27285d22b82d45c088821f5dab50c10f3cdf3ba
Puttiphong Buala <puttiphong.b@eest-es.com>
2026-03-18 15:36:46 +0700

    first commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON
+const SUPABASE_URL  = 'https://eqgtyveijshkljffnrvf.supabase.co'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZ3R5dmVpanNoa2xqZmZucnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MDUyMDMsImV4cCI6MjA4OTM4MTIwM30.WNRr_wwJTcSOo_tR8JFHeHcI5tzGgD1vROvaqPFajao'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
